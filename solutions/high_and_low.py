edd2c1d147d979abdcc777ee3ebb06c414898adc
Biola <52200708+decorouz@users.noreply.github.com>
2021-09-24 12:04:12 +0100

    Implement kata

---
+++ solutions/high_and_low.py
@@ -0,0 +1,15 @@
+def high_and_low(numbers: str):
+    """Given a string of space seperated numbers.
+    return the highest and lowest
+
+    Args:
+        numbers (string): string of numbers
+    """
+    # convert to string
+    numbers = [int(x) for x in numbers.split()]
+    return f"{max(numbers)} {min(numbers)}"
+
+
+print(high_and_low("8 3 -5 42 -1 0 0 -9 4 7 4 -4"))
+# high_and_low("1 2 3 4 5")
+# high_and_low("1 9 3 4 -5")
